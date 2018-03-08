d0d11ea8a86e0e9977e86c647c423b10b3d4fbfd
Vinicius Rocha <viniciussr@gmail.com>
2018-03-07 23:02:48 -0300

    Show information when there isn't connection. Initialize test with maps.

--- app/src/main/java/com/capstone/runapp/MapActivity.java
+++ app/src/main/java/com/capstone/runapp/MapActivity.java
@@ -1,24 +1,174 @@
 package com.capstone.runapp;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.capstone.runapp.model.Events;
 import com.capstone.runapp.service.DisposableManager;
+import com.capstone.runapp.service.EventService;
+import com.capstone.runapp.service.ServiceFactory;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
-public class MapActivity extends AppCompatActivity {
+import static android.content.ContentValues.TAG;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
+
+
+    private static final int DEFAULT_ZOOM = 15;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    // Keys for storing activity state.
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
+    // Used for selecting the current place.
+    private static final int M_MAX_ENTRIES = 5;
+    // A default location (Sydney, Australia) and default zoom to use when location permission is
+    // not granted.
+    private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085);
+    // private static final String TAG = MapsActivityCurrentPlace.class.getSimpleName();
+    private GoogleMap mMap;
+    private CameraPosition mCameraPosition;
+    // The entry points to the Places API.
+    private GeoDataClient mGeoDataClient;
+    private PlaceDetectionClient mPlaceDetectionClient;
+    // The entry point to the Fused Location Provider.
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private boolean mLocationPermissionGranted;
+    // The geographical location where the device is currently located. That is, the last-known
+    // location retrieved by the Fused Location Provider.
+    private Location mLastKnownLocation;
+    private String[] mLikelyPlaceNames;
+    private String[] mLikelyPlaceAddresses;
+    private String[] mLikelyPlaceAttributions;
+    private LatLng[] mLikelyPlaceLatLngs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         ButterKnife.bind(this);
+
+        if (isOnline()) {
+            load();
+        } else {
+            showErrorMessage();
+        }
     }
 
     @Override
     protected void onDestroy() {
         DisposableManager.dispose();
         super.onDestroy();
+    }
+
+
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+    private void showErrorMessage() {
+        Snackbar.make(findViewById(android.R.id.content), "No internet conection", Snackbar.LENGTH_INDEFINITE).setAction("RETRY", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recreate();
+            }
+        }).show();
+    }
+
+
+    private void load() {
+
+        EventService service = ServiceFactory.create(EventService.class, EventService.ENDPOINT);
+        Observable<Events> observable = service.getEvents();
+
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Events>() {
+
+                               Events postEvents;
+
+                               @Override
+                               public void onSubscribe(Disposable d) {
+                                   DisposableManager.add(d);
+                               }
+
+                               @Override
+                               public void onNext(Events events) {
+                                   Log.d(TAG, "In onNext()");
+                                   postEvents = events;
+                               }
+
+                               @Override
+                               public void onError(Throwable e) {
+                                   e.printStackTrace();
+                                   Log.d(TAG, "In onError()");
+                               }
+
+                               @Override
+                               public void onComplete() {
+                                   Log.d(TAG, "In onCompleted()");
+                                   Toast toast = Toast.makeText(getApplicationContext(), postEvents.items().get(0).nome(), Toast.LENGTH_LONG);
+                                   toast.show();
+                               }
+                           }
+                );
+    }
+
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
